40e648f43e651b4791ed1d8936a2b3305b3854a0
Sasha Ivanova <sashaivanova.pro@gmail.com>
2024-08-08 21:03:51 +0500

    feat: partial solution for conditions tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -259,8 +259,23 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+
+function isCreditCardNumber(ccn) {
+  const stringNumber = ccn.toString();
+  let result = 0;
+  let toDouble = false;
+  for (let i = stringNumber.length - 1; i >= 0; i -= 1) {
+    let digit = parseInt(stringNumber[i], 10);
+    if (toDouble) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    result += digit;
+    toDouble = !toDouble;
+  }
+  return result % 10 === 0;
 }
 
 /**
